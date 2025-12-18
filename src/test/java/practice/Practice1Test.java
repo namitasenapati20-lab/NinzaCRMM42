a9fac7308f6772aef3192a7d0305f3c5bc64ed33
namita <namita@192.168.1.51>
2025-12-18 07:44:11 +0530

    First time commited

---
+++ src/test/java/practice/Practice1Test.java
@@ -0,0 +1,16 @@
+package practice;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Practice1Test {
+	
+	@Test(groups="smoke")
+	
+	public void Booking()
+	{
+		System.out.println("Oneway booking");
+	}
+
+}
---
+++ src/test/java/practice/Practice2Test.java
@@ -0,0 +1,16 @@
+package practice;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Practice2Test {
+	
+@Test(groups="regression")
+	
+	public void Cancellation()
+	{
+		System.out.println("Booking cancelled");
+	}
+
+}
